2ab99f5f4a9af31429137964ed78e471f7c8959d
felipe montefuscolo <felipe.mt87@gmail.com>
2016-01-30 16:12:26 +0000

    add sort function

--- src/impressions.py
+++ src/impressions.py
@@ -3,29 +3,35 @@ import time
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+from pyspark.sql import SQLContext # needed to transform PipelinedRDD to RDD for the functions sortByKey
+import pyspark
 
 if __name__ == "__main__":
 
     # Create a local StreamingContext with two working thread and batch interval of 1 second
     sc = SparkContext("local[2]", "NetworkWordCount")
     ssc = StreamingContext(sc, 1)
+    sql = SQLContext(sc)
    
     lines = sc.textFile("test.txt", 1)
-    #counts = lines.flatMap(lambda x: x.split(' ')) \
-    #              .map(lambda x: (x, 1)) \
-    #              .reduceByKey(add)
-    
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(lambda a, b: a + b) \
+                  .sortByKey(True) 
+    # Try this after .reduceByKey: .map(lambda x:(x[0],x[1])) \ # which is faster? I don't know   
+ 
     # Create the queue through which RDDs can be pushed to
     # a QueueInputDStream
     rddQueue = []
-    rddQueue.append(lines)
+    rddQueue.append(counts)
     
     # Create the QueueInputDStream and use it do some processing
     inputStream = ssc.queueStream(rddQueue)
-    mappedStream = inputStream.flatMap(lambda x: x.split(' ')) \
-                              .map(lambda x: (x, 1))
-    reducedStream = mappedStream.reduceByKey(lambda a, b: a + b)
-    reducedStream.pprint()
+    #mappedStream = inputStream.flatMap(lambda x: x.split(' ')) \
+    #                          .map(lambda x: (x, 1))
+    #reducedStream = mappedStream.reduceByKey(lambda a, b: a + b)
+    #sortedStream = reducedStream.sortByKey()
+    inputStream.pprint()
     
     logger = sc._jvm.org.apache.log4j
     logger.LogManager.getLogger("org").setLevel( logger.Level.OFF )
@@ -33,7 +39,10 @@ if __name__ == "__main__":
     
     #ssc.start()             # Start the computation
     #ssc.awaitTermination()  # Wait for the computation to terminate
-    
+   
+     
+
+ 
     ssc.start()
     time.sleep(6)
     ssc.stop(stopSparkContext=True, stopGraceFully=True)
